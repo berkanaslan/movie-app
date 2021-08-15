1605e47ca2485d36aa8823ee6044c59ba0918a1b
Berkan ASLAN <aslnberkan@gmail.com>
2021-08-16 00:47:16 +0300

    Angular services example, using alertify, injectable etc...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
-import { CategoryComponent } from './category/category.component';
-import { MoviesComponent } from './movies/movies.component';
-import { MovieComponent } from './movies/movie/movie.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { FooterComponent } from './footer/footer.component';
+import {CategoryComponent} from './category/category.component';
+import {MoviesComponent} from './movies/movies.component';
+import {MovieComponent} from './movies/movie/movie.component';
+import {MovieDetailsComponent} from './movie-details/movie-details.component';
+import {FooterComponent} from './footer/footer.component';
 import {SummaryPipe} from "./pipes/summary.pipe";
 import {FormsModule} from "@angular/forms";
 import {MovieFilterPipe} from "./pipes/movie.filter.pipe";
+import {AlertifyService} from "./services/alertify.service";
 
 @NgModule({
   declarations: [
@@ -28,7 +29,9 @@ import {MovieFilterPipe} from "./pipes/movie.filter.pipe";
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    AlertifyService
+  ],
   bootstrap: [AppComponent]
 })
 
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Movie} from "../model/movie";
 import {MovieRepository} from "../model/movie.repository";
+import {AlertifyService} from "../services/alertify.service";
 
 @Component({
   selector: 'app-movies',
@@ -16,7 +17,7 @@ export class MoviesComponent implements OnInit {
 
   inputText: string = "";
 
-  constructor() {
+  constructor(private alertify: AlertifyService) {
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
     this.filteredMovies = this.movies;
@@ -33,4 +34,19 @@ export class MoviesComponent implements OnInit {
         .filter(movie => movie.title.indexOf(this.inputText) !== -1 || movie.description.indexOf(this.inputText) !== -1)
       : this.movies;
   }
+
+  onAddToListClicked($event: any, movie: Movie) {
+    if ($event.target.classList.contains("btn-primary")) {
+      $event.target.classList.remove("btn-primary");
+      $event.target.classList.add("btn-danger");
+      $event.target.innerText = "Remove from list";
+      this.alertify.success(movie.title + " added to list.");
+    } else {
+      $event.target.classList.remove("btn-danger");
+      $event.target.classList.add("btn-primary");
+      $event.target.innerText = "Add to list";
+      this.alertify.error(movie.title + " removed from list.");
+    }
+  }
+
 }
