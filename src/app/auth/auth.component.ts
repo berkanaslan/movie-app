25d65ed54c8742cf7c27bbb701571afef51cb6b3
Berkan ASLAN <aslnberkan@gmail.com>
2021-08-31 23:28:06 +0300

    Auth service initialized.
    Sign-up ok.
    Sign-in ok.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,24 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {finalize} from "rxjs/operators";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.css']
+    styleUrls: ['./auth.component.css'],
+    providers: [AuthService]
 })
 export class AuthComponent implements OnInit {
     isLoginMode: boolean = false;
+
     formGroup: FormGroup = new FormGroup({
         emailController: new FormControl("", [Validators.required, Validators.email]),
         passwordController: new FormControl("", [Validators.required])
     });
 
-    constructor() {
+    authResponse: Observable<User>;
+
+    isLoading: boolean = false;
+
+    constructor(private authService: AuthService) {
     }
 
     ngOnInit(): void {
     }
-
 
     onToggleChanged() {
         this.isLoginMode = !this.isLoginMode;
@@ -29,7 +38,16 @@ export class AuthComponent implements OnInit {
             return;
         }
 
-        console.log(this.formGroup.value);
+        this.isLoading = true;
+        if (this.isLoginMode) {
+            this.authResponse = this.authService.signIn(this.formGroup.value.emailController, this.formGroup.value.passwordController);
+        } else {
+            this.authResponse = this.authService.signUp(this.formGroup.value.emailController, this.formGroup.value.passwordController);
+        }
 
+        console.log(this.authResponse.subscribe(value => {
+            console.log(value);
+            this.isLoading = false;
+        }, error => this.isLoading = false));
     }
 }
